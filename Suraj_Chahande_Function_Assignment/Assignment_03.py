deee5d97dc2065b95665c5e07e7fd17a4106ea09
Suraj Chahande <chahandesuraj@gmail.com>
2021-05-03 10:50:23 +0530

    some changes

--- Suraj_Chahande_Function_Assignment/Assignment_03.py
+++ Suraj_Chahande_Function_Assignment/Assignment_03.py
@@ -1,10 +1,16 @@
 # Write a program for mathematical operations in a single function.
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-def Math(num1,num2):
-    return (num1 + num2)
-    return (num1 - num2)
-    return (num1 * num2)
-    return (num1 / num2)
-Math(num1 + num2)
+
+def Math():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    add = num1 + num2
+    print("The additon of two numbers is: ",add)
+    sub = num1 - num2
+    print("The substraction of two numbers is: ",sub)
+    mul = num1 * num2
+    print("The multiplication of two numbers is: ",mul)
+    div = num1 / num2
+    print("The division of two numbers is: ",div)
+    
+Math()
     
